346c88c635aaadf6da61292865e5ca7ad7cb19b3
idavidlin <linyongxi7@gmail.com>
2019-03-27 11:42:17 +1100

    Set up webpack

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class MyTodo extends Component {
+  render() {
+    return (<div>hehehe</div>)
+  }
+}
+
+ReactDOM.render(
+  <MyTodo />,
+  document.getElementById('root')
+);
